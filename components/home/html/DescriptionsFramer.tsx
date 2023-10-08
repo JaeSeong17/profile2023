b70c97d3b9d03598d8a64b78defb9324c0516e07
AnJaeSeong <dktmzh6@gmail.com>
2023-10-08 14:16:20 +0900

    FEAT: Description의 scroll progress indicator 추가
    - 각 설명 박스의 타이틀 옆에 원 형태의 scroll indicator 추가

---
+++ components/home/html/DescriptionsFramer.tsx
@@ -0,0 +1,110 @@
+import projectsData from '@/public/static/projectsData';
+import {
+  Variants,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from 'framer-motion';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const fadeVariants: Variants = {
+  fadeOn: {
+    opacity: 1,
+    visibility: 'visible',
+    transition: {
+      duration: 0.5,
+    },
+  },
+  fadeOff: {
+    opacity: 0,
+    visibility: 'hidden',
+    transition: {
+      duration: 0.5,
+      visibility: { delay: 0.5 },
+    },
+  },
+};
+
+export default function DescriptionsFramer() {
+  const { scrollY } = useScroll();
+  const [hookedYPosition, setHookedYPosition] = useState(0);
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    setHookedYPosition(latest);
+  });
+  const start = 15000;
+  const interval = 4000;
+  const length = 3000;
+
+  return (
+    <div className='fixed w-screen h-screen flex items-end justify-end'>
+      {projectsData.map((data, idx) => (
+        <motion.div
+          key={idx}
+          className='absolute right-[6%] sm:right-[10%] bottom-[10%]
+          w-[88%] sm:w-[80%] max-w-[500px] p-[20px]
+          bg-neutral-900/80 rounded-2xl shadow-[0_0_30px_-4px]
+          text-white
+          flex flex-col gap-5
+          '
+          initial={'fadeOff'}
+          animate={
+            hookedYPosition >= start + interval * idx &&
+            hookedYPosition <= start + interval * idx + length
+              ? 'fadeOn'
+              : 'fadeOff'
+          }
+          variants={fadeVariants}
+        >
+          <div className='text-5xl flex justify-between'>
+            {data.title}
+
+            <svg className='w-[34px] h-[34px]'>
+              <motion.circle
+                cx='17'
+                cy='17'
+                r='12'
+                stroke='white'
+                strokeWidth={5}
+                fill='none'
+                pathLength='1'
+                initial={{ pathLength: 0, rotate: -90 }}
+                animate={{
+                  pathLength:
+                    hookedYPosition < start + interval * idx
+                      ? 0
+                      : ((hookedYPosition - (start + interval * idx)) /
+                          interval) *
+                        1.4,
+                }}
+                transition={{
+                  duration: 0.3,
+                }}
+              />
+            </svg>
+          </div>
+          <hr />
+
+          <div className='text-3xl'>{data.desc}</div>
+          <div className='flex flex-wrap gap-2 mt-1'>
+            {data.skills.map((skill, idx) => (
+              <div className='text-2xl border-2 rounded-lg p-1' key={idx}>
+                {skill}
+              </div>
+            ))}
+          </div>
+          <div
+            className='
+            border border-white px-4 py-2 rounded-2xl self-end
+            transition hover:scale-110
+            '
+          >
+            <Link href={`/projects/${data.label}`} className='text-2xl'>
+              자세히
+            </Link>
+          </div>
+        </motion.div>
+      ))}
+    </div>
+  );
+}
