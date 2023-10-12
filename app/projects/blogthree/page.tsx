e685bf6931db404b61b8758011ca2885a5bb72bd
AnJaeSeong <dktmzh6@gmail.com>
2023-10-13 01:47:24 +0900

    FEAT: 프로젝트 개별 페이지 추가

---
+++ app/projects/blogthree/page.tsx
@@ -0,0 +1,6 @@
+import ProjectWrapper from '@/components/projects/ProjectWrapper';
+import projectsData from '@/public/static/projectsData';
+
+export default function Home() {
+  return <ProjectWrapper data={projectsData[0]} />;
+}
