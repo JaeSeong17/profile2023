c34c0051d9b95b08fe5f4dbc315964ad30b9be64
AnJaeSeong <dktmzh6@gmail.com>
2023-10-15 13:37:22 +0900

    FIX: Gallery에서 styled-components import 부분 제거

--- components/projects/Gallery.tsx
+++ components/projects/Gallery.tsx
@@ -1,7 +1,6 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useState } from 'react';
-import styled from 'styled-components';
 
 interface GalleryParams {
   label: string;
