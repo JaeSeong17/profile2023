31f23aac8e237ffabdd0c969957c02273eb9b845
AnJaeSeong <dktmzh6@gmail.com>
2023-10-20 19:29:42 +0900

    STYLE: 프로필 가로선 조건부 스타일 적용 제거
    - 프로필 영역의 가로선을 스크린모드에 따라 조건부로 구현했으나 적용되지 않는 오류 때문에 하나의 스타일로 통일하도록 변경

--- components/profile/HorizontalLine.tsx
+++ components/profile/HorizontalLine.tsx
@@ -2,8 +2,7 @@ import useScreenModeStore from '@/lib/modules/screenMode';
 import { motion } from 'framer-motion';
 
 export default function HorizontalLine() {
-  const screenMode = useScreenModeStore((state) => state.screenMode);
-  return screenMode === 'MobileVertical' ? (
+  return (
     <svg className='w-full h-[2px] overflow-hidden'>
       <defs>
         <linearGradient id='horizontal' x1='0%' y1='0%' x2='100%' y2='0%'>
@@ -13,7 +12,9 @@ export default function HorizontalLine() {
           <stop offset='100%' stopColor='white' stopOpacity={0} />
         </linearGradient>
       </defs>
+
       <motion.rect
+        id='mob'
         x={0}
         y={0}
         width={'100%'}
@@ -21,37 +22,10 @@ export default function HorizontalLine() {
         fill={'url(#horizontal)'}
         variants={{
           animOn: {
-            scale: 0,
-          },
-          animOff: {
             scale: 1,
           },
-        }}
-        transition={{
-          duration: 1,
-        }}
-      />
-    </svg>
-  ) : (
-    <svg className='w-full h-[2px] overflow-hidden'>
-      <defs>
-        <linearGradient id='horizontal' x1='0%' y1='0%' x2='100%' y2='0%'>
-          <stop offset='60%' stopColor='white' stopOpacity={1} />
-          <stop offset='100%' stopColor='white' stopOpacity={0} />
-        </linearGradient>
-      </defs>
-      <motion.rect
-        x={0}
-        y={0}
-        width={'100%'}
-        height={'100%'}
-        fill={'url(#horizontal)'}
-        variants={{
-          animOn: {
-            x: '0%',
-          },
           animOff: {
-            x: '-100%',
+            scale: 0,
           },
         }}
         transition={{
